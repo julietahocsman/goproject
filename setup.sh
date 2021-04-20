3abb00b2a853be13d49655ecac4eed0c70661005
Julieta Hocsman <julihocsman@gmail.com>
2021-04-20 09:12:41 -0300

    adding email

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"${julihocsman@gmail.com}\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
-port = $PORT\n\
 enableCORS = false\n\
-\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
